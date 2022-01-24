92906db50f7518a2a87a7ea3751e3c80a3d44e8d
juanglbnt <juanguillermo.loaiza@globant.com>
2022-01-23 22:21:13 -0500

    progress

--- Topic0/src/MemoryManagement.java
+++ Topic0/src/MemoryManagement.java
@@ -10,26 +10,25 @@ public class MemoryManagement {
 
         memoryObj.startMemory();
         memoryObj.systemProcess();
-
         memoryObj.showMemory();
-        System.out.println("system processes\n" + memoryObj.returnSystemProcesses());
-        System.out.println("is s001 in memory currently?: " + memoryObj.searchProcess(memoryObj.systemProcesses, "s005"));
+
+
 
     }
 
-    public void startMemory(){
+    public void startMemory() {
 
-        for(int row = 0; row < memory.length; row++){
-            for(int col = 0; col < memory[row].length; col++){
+        for (int row = 0; row < memory.length; row++) {
+            for (int col = 0; col < memory[row].length; col++) {
                 memory[row][col] = "****";
             }
         }
     }
 
-    public void showMemory(){
+    public void showMemory() {
 
-        for(int row = 0; row < memory.length; row++){
-            for(int col = 0; col < memory[row].length; col++){
+        for (int row = 0; row < memory.length; row++) {
+            for (int col = 0; col < memory[row].length; col++) {
                 System.out.print(memory[row][col] + " ");
             }
             System.out.println();
@@ -39,57 +38,77 @@ public class MemoryManagement {
     public void systemProcess(){
         int size;
         int id = 0;
-        String idProcesss = "s00" + id;
+        String idProcesss;
         size = 5 + random.nextInt(16 - 5);
         //parametros de prueba
-        memory[3][9] = "s000";
+        /*memory[3][9] = "s000";
         memory[0][2] = "s001";
-        memory[0][3] = "s002";
+        memory[0][1] = "s002";
+        memory[0][4] = "s003";
+        memory[0][3] = "s004";
+        memory[0][5] = "s005";
+        memory[0][6] = "s006";
+        memory[1][1] = "s007";
+        memory[1][2] = "s008";
+        memory[1][3] = "s009";
+        memory[1][4] = "s010";*/
 
-        for(int row = 0; row < memory.length; row++){
-            for(int col = 0; col < memory[row].length; col++){
-                if(memory[row][col].contains(idProcesss)){
-                    id++;
+        idProcesss = "s00" + id;
+
+        while(searchProcess(idProcesss)){
+            id++;
+            if(id > 9){
+                idProcesss = "s0" + id;
+            }else{
+                idProcesss = "s00" + id;
+            }
+
+        }
+        System.out.println("THE NEW PROCESS IN MEMORY IS: " + idProcesss);
+        System.out.println("with size: " + size);
+
+        for(int i = 0; i < size; i++){
+            for(int row = 0; row < memory.length; row++){
+                for(int col = 0; col < memory[row].length; col++){
+                    memory[row][col] = idProcesss;
                 }
             }
         }
-        if(id > 10){
-            idProcesss = "s0" + id;
-        }else{
-            idProcesss = "s00" + id;
-        }
 
-        System.out.println("the process " + idProcesss + " is already\n in memory THE NEW PROCESS IS: " + idProcesss);
     }
 
-    public void aplicationProcess(){
+    public void aplicationProcess() {
         int size;
         size = 10 + random.nextInt(21 - 10);
 
 
     }
 
-    public String returnSystemProcesses(){
+    /*idElement = "s00" + id;
+        System.out.println("ID TO CHECK: " + idElement);
+        for(int row = 0; row < matrix.length; row++){
+            for(int col = 0; col < matrix[row].length; col++){
+                if(matrix[row][col].equalsIgnoreCase(idElement)){
+                    id++;
+                }
+            }
 
-        for(int row = 0; row < memory.length; row++){
+        }*/
+
+    public boolean searchProcess(String id) {
+        int counter = 0;
+        boolean valid = false;
+
+        for(int row = 0; row < memory.length; row++) {
             for(int col = 0; col < memory[row].length; col++){
-                if(memory[row][col].contains("s")){
-                    systemProcesses += memory[row][col] + " ";
+                if(memory[row][col].equalsIgnoreCase(id)){
+                    counter++;
                 }
             }
         }
-        return systemProcesses;
-    }
 
-    public boolean searchProcess(String processes, String id){
-        boolean valid = false;
-        for(int i = 0; i < processes.length(); i++){
-            if(processes.contains(id)){
-                valid = true;
-            }
-        }
+        valid = (counter > 0);
         return valid;
+
     }
-
-
 }
