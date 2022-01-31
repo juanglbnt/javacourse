0d88d1de759319d734e373032880baa7bb96596b
juanglbnt <juanguillermo.loaiza@globant.com>
2022-01-30 20:04:34 -0500

    builder pattern

---
+++ semana2Topic1/BuilderPatternExample/src/main/java/com/juan/designPatterns/BuilderPatternHouseExample/Main.java
@@ -0,0 +1,4 @@
+package com.juan.designPatterns.BuilderPatternHouseExample;
+
+public class Main {
+}
---
+++ semana2Topic1/BuilderPatternExample/src/main/java/com/juan/designPatterns/BuilderPatternHouseExample/product/House.java
@@ -0,0 +1,9 @@
+package com.juan.designPatterns.BuilderPatternHouseExample.product;
+
+public class House {
+    private String foundation;
+    private String structure;
+    private String roof;
+    private boolean furnished;
+    private boolean painted;
+}
