520d7d50e6871ce7258273fd289f0966e118cfcb
juanglbnt <juanguillermo.loaiza@globant.com>
2022-02-07 12:08:56 -0500

    practica spring

---
+++ semana3Topic2/SpringEjemplos/demo/src/main/java/com/example/demo/dao/PersonDataAccess.java
@@ -0,0 +1,22 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/*nota: esto nos crea un bean, lo que
+nos permite intectarlo en otras clases*/
+@Repository("fakeDao")
+public class PersonDataAccess implements PersonDAO {
+
+    private static List<Person> db = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        db.add(new Person(id, person.getName()));
+        return 1;
+    }
+}
---
+++ semana3Topic2/SpringEjemplos/demo/src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.PersonDAO;
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    private final PersonDAO personDAO;
+
+    /*aqui se esta haciendo la inyeccion*/
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    public int addPerson(Person person){
+        return personDAO.insertPerson(person);
+    }
+}
