79b9f100235db5f26d536a818987868952b738dd
juanglbnt <juanguillermo.loaiza@globant.com>
2022-02-11 18:31:49 -0500

    topic 3 examples

--- semana4topic3/FunctionalProgrammingExercises/src/main/java/com/juan/topic3functionalprogramming/annotations/AnnotationsApp.java
+++ semana4topic3/FunctionalProgrammingExercises/src/main/java/com/juan/topic3functionalprogramming/annotations/AnnotationsApp.java
@@ -1,4 +1,34 @@
 package com.juan.topic3functionalprogramming.annotations;
 
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class AnnotationsApp {
+
+    public static void main(String[] args) {
+        Language[] arrayLanguages = ProgrammingLanguage.class.getAnnotationsByType(Language.class);
+        System.out.println("there is " + arrayLanguages.length + " annotations");
+
+        Languages lang = ProgrammingLanguage.class.getAnnotation(Languages.class);
+        for(Language language:lang.value()){
+            System.out.println(language.value());
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Languages {
+        Language[] value() default {};
+    }
+
+    @Repeatable(value = Languages.class)
+    public @interface Language {
+        String value();
+    }
+
+    @Language("java")
+    @Language("c")
+    public interface ProgrammingLanguage{
+
+    }
 }
