dcfca24dfcea910640066c1087f0e9f8a97c5a22
juanglbnt <juanguillermo.loaiza@globant.com>
2022-02-19 01:49:07 -0500

    topic 3

--- semana4topic3/Topic3/src/main/java/Main.java
+++ semana4topic3/Topic3/src/main/java/Main.java
@@ -1,16 +1,15 @@
 import com.juan.topic3.challenge.Exercise1;
 import com.juan.topic3.challenge.Exercise2;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         Exercise2 ex2 = new Exercise2();
         Exercise1 ex1 = new Exercise1();
+        Exercise2 exercise2 = new Exercise2();
 
-        //ex1.getTheExpansion();
-        ex2.getPasswords();
+        ex1.getTheExpansion();
+        exercise2.execution();
 
     }
 }
--- semana4topic3/Topic3/src/main/java/com/juan/topic3/challenge/Exercise2.java
+++ semana4topic3/Topic3/src/main/java/com/juan/topic3/challenge/Exercise2.java
@@ -1,70 +1,105 @@
 package com.juan.topic3.challenge;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Exercise2 {
+    List<List> passwordMap = new ArrayList<>();
 
     public Exercise2() {
+
     }
+
+    Scanner in = new Scanner(System.in);
 
     public Predicate<Integer> validNumberOfTestCases = n -> n >= 1 && n <= 10;
     public Predicate<Integer> validNumberOfPasswords = n -> n >= 1 && n <= 10;
 
-    public void getPasswords() {
-        Scanner scan = new Scanner(System.in);
-        int testCasesNumber;
+    public void execution() {
+        getPasswords();
+        computeCracker();
+    }
+
+    public void computeCracker() {
+        List<String> passwords;
+        String attempt;
+
+        for (int i = 0; i < passwordMap.size(); i++) {
+            List<String> subMap = new ArrayList<>();
+            subMap = passwordMap.get(i);
+
+            attempt = subMap.get(subMap.size()-1);
+            passwords = subMap.subList(0,subMap.size()-1);
+
+            //para efectos de prueba
+           /* System.out.println("attempt " + i + attempt);
+            passwords.forEach(System.out::println);
+            System.out.println("///");*/
+
+            List<String> resultPasswords = passwordCracker(passwords, attempt);
+            String achieved = resultPasswords.stream().collect(Collectors.joining());
+
+            if (achieved.equals(attempt)) {
+                System.out.println(resultPasswords.stream().collect(Collectors.joining(" ")));
+            }else {
+                System.out.println("error, wrong password");
+            }
+            System.out.println("");
+        }
+    }
+
+    public List<String> passwordCracker(List<String> passwords, String attempt) {
+        return passwords.stream()
+                .filter(attempt::startsWith)
+                .flatMap(password -> Stream.concat(Stream.of(password),
+                        passwordCracker(passwords, attempt.substring(password.length()))
+                                .stream()))
+                .collect(Collectors.toList());
+    }
+
+    public List<List> getPasswords() {
+        int numberOfCases;
         int numberOfPasswords;
         String password;
-        String loginAttempt;
-        List<String> passwordList = new ArrayList<>();
-        Map<String, List<String>> passwordMap = new HashMap<>();
+        String attempt;
 
-        do {
-            System.out.print("Enter the number of cases\n" +
-                    "a number between 1 and 10: ");
-            testCasesNumber = scan.nextInt();
+        do{
+            System.out.print("enter the number of test cases [1 - 10]: ");
+            numberOfCases = in.nextInt();
+        }while (!validNumberOfTestCases.test(numberOfCases));
 
-        }while (!validNumberOfTestCases.test(testCasesNumber));
-
-        for(int i = 0; i < testCasesNumber; i++) {
-
+        for(int i = 0; i < numberOfCases; i++) {
+            List<String> passwordList = new ArrayList<>();
             do {
-                System.out.println("enter the number of passwords\n" +
-                        "A number between 1 and 10: ");
-                numberOfPasswords = scan.nextInt();
-            }while (!validNumberOfPasswords.test(numberOfPasswords));
+                System.out.print("enter the number of passwords [1 - 10]: ");
+                numberOfPasswords = in.nextInt();
+            }while(!validNumberOfPasswords.test(numberOfPasswords));
 
             for(int j = 0; j < numberOfPasswords; j++) {
-                password = scan.next();
+                password = in.next();
                 passwordList.add(password);
             }
 
-            System.out.print("");
-            loginAttempt = scan.next();
-            passwordMap.put(loginAttempt, passwordList);
-            passwordList.clear();
-        }
-        printMap(passwordMap);
+            attempt = in.next();
+            passwordList.add(attempt);
+            passwordMap.add(passwordList);
 
+        }
+        return passwordMap;
     }
 
-    public void printMap(Map<String, List<String>> map) {
-        Iterator it = map.keySet().iterator();
-
-        while(it.hasNext()) {
-            String key = it.next().toString();
-            System.out.println("attempt: " + key + " passwords " + map.get(key));
+    /*
+    * para efectos de prueba
+    * en realidad no se trabaja con mapas en este ejercicio
+    * sino que utilizo una lista de listas que pretendo ver como un hashmap
+    * o algo asi*/
+    public void printMap(List<List> map) {
+        for(int i = 0; i < map.size(); i++) {
+            System.out.println(i + map.get(i).toString());
         }
     }
-
-    public String verLista(List<String> list) {
-        String lista = "";
-
-        for (String element : list) {
-            lista += element + " ";
-        }
-        return lista;
-    }
-
 }
