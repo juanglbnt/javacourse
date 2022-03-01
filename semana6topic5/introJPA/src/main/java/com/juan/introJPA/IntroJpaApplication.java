4070f2b3546de5e9948b79b2d22c89535f371428
juanglbnt <juanguillermo.loaiza@globant.com>
2022-03-01 15:36:18 -0500

    topic 5

---
+++ semana6topic5/introJPA/src/main/java/com/juan/introJPA/IntroJpaApplication.java
@@ -0,0 +1,13 @@
+package com.juan.introJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroJpaApplication.class, args);
+	}
+
+}
