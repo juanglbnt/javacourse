5886ac73b8fded4878154ee1480aba771f7f4983
juanglbnt <juanguillermo.loaiza@globant.com>
2022-03-03 01:03:51 -0500

    topic 5

---
+++ semana6topic5/hibernateexample/src/test/java/com/juan/hibernateexample/HibernateexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.hibernateexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
