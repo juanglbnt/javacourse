c7f4ee14247bb9e0fec2af3026c68a1ee5e7274c
juanglbnt <juanguillermo.loaiza@globant.com>
2022-03-03 15:35:04 -0500

    hibernate example

--- semana6topic5/hibernateexample/src/main/resources/import.sql
+++ semana6topic5/hibernateexample/src/main/resources/import.sql
@@ -1,3 +1,5 @@
-insert into book values(1, 'steppenwold');
+insert into book values(1, 'steppenwolf');
 insert into book values(2, 'nausea');
 insert into book values(3, 'chronicle of a death foretold');
+insert into book values(4, 'crime and punishment');
+insert into book values(5, 'post office');
