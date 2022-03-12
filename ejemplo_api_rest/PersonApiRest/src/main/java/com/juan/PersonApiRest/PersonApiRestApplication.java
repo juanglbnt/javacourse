468c2f2d0f6d04ed05bc47874a8fe559180885d3
juanglbnt <juanguillermo.loaiza@globant.com>
2022-03-12 01:27:00 -0500

    ejemplo api rest

---
+++ ejemplo_api_rest/PersonApiRest/src/main/java/com/juan/PersonApiRest/PersonApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.juan.PersonApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiRestApplication.class, args);
+	}
+
+}
---
+++ ejemplo_api_rest/PersonApiRest/src/test/java/com/juan/PersonApiRest/PersonApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.PersonApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
