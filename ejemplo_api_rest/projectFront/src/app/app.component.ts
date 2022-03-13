57d048124da48f46386f7d7549d42754943f60f1
juanglbnt <juanguillermo.loaiza@globant.com>
2022-03-13 02:20:08 -0500

    ejercicio api rest front

--- ejemplo_api_rest/projectFront/src/app/app.component.ts
+++ ejemplo_api_rest/projectFront/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { CountryService } from './services/country/country.service';
 import { StateService } from './services/state/state.service';
 import { PersonService } from './services/person/person.service';
@@ -9,26 +10,37 @@ import { PersonService } from './services/person/person.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   personForm: FormGroup;
+  countries: any;
 
   constructor(
     public fb: FormBuilder,
     public stateService: StateService,
     public countryService: CountryService,
     public personService: PersonService
-  ){
+  ) {
 
   }
   ngOnInit(): void {
     this.personForm = this.fb.group({
-      name : ['', Validators.required],
-      surname : ['', Validators.required],
-      age : ['', Validators.required],
-      country : ['', Validators.required],
-      state : ['', Validators.required],
-    })
-}
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      country: ['', Validators.required],
+      state: ['', Validators.required],
+    });;
+
+    this.countryService.getAllCountries().subscribe(resp => {
+      this.countries = resp
+      console.log(resp)
+    },
+      error => { console.error(error) })
+  }
+
+  save(): void {
+
+  }
 
 }
--- ejemplo_api_rest/projectFront/src/app/services/country/country.service.ts
+++ ejemplo_api_rest/projectFront/src/app/services/country/country.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
-  constructor() { }
+  private API_SERVER = "http://localhost:8080/country"
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public getAllCountries(): Observable<any> {
+    return this.httpClient.get(this.API_SERVER)
+  }
 }
