b04d0b37666c90c096b0efaed6f0341aed0c3276
juanglbnt <juanguillermo.loaiza@globant.com>
2022-03-24 03:17:46 -0500

    -

--- semana8topic7/topic7/src/main/java/com/juan/topic7/repository/WeatherDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.juan.topic7.repository;
-
-import com.juan.topic7.model.WeatherData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WeatherDataRepository extends JpaRepository<WeatherData, Integer> {
-}
