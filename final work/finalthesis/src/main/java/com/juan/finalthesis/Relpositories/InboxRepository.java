c6a2c215b1f64c563eaacfa649f691d81a91b21b
juanglbnt <juanguillermo.loaiza@globant.com>
2022-03-29 22:44:13 -0500

    a

--- final work/finalthesis/src/main/java/com/juan/finalthesis/Relpositories/InboxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.juan.finalthesis.Relpositories;
-
-import com.juan.finalthesis.model.Inbox;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InboxRepository extends JpaRepository<Inbox, Long> {
-}
